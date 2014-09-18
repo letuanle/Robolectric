8012c03a45067a25f87c8d832da14930bc5d8e57
Tuan Le <ltmailrac@yahoo.com>
2014-09-18 15:39:36 +0700

    0003

--- CalculatorTest/src/lt/CalculatorTest.java
+++ CalculatorTest/src/lt/CalculatorTest.java
@@ -8,23 +8,13 @@ import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
-import org.robolectric.annotation.Implementation;
-import org.robolectric.annotation.Implements;
-import org.robolectric.annotation.RealObject;
 import org.robolectric.shadows.ShadowTextView;
 import com.calculator.CalculatorActivity;
 import com.calculator.R;
 
-import android.content.Context;
-import android.telephony.SmsManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
-@Implements(TextView.class)
-class CustomShadowTextView extends ShadowTextView {
-  @RealObject private TextView tv;
-}
 
 @Config(manifest = "../Calculator/AndroidManifest.xml")
 @RunWith(RobolectricTestRunner.class)
@@ -55,10 +45,9 @@ public class CalculatorTest {
     assertEquals("54", s);
   }
   
- @Test
- @Config(shadows={CustomShadowTextView.class})
+@Test
  public void testClick_TalktoShadow() {
-	 CustomShadowTextView tb = shadowOf_(total);
+	 ShadowTextView tb = shadowOf_(total);
 	  
 	  String startedIntent = tb.innerText();
 	  assertEquals(total.getText().toString(), startedIntent);		
